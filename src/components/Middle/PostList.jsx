660705a6bdf3f84ac47fe8406be8b07eac481e8e
Bledon <bledonqyqalla@gmail.com>
2024-02-01 22:51:44 +0100

    Cleanup an aile 2

--- src/components/Middle/PostList.jsx
+++ src/components/Middle/PostList.jsx
@@ -2,19 +2,15 @@ import React from 'react'
 import styles from '../Post/post.module.css'
 
 
-export default function PostList({posts}) {
+export default function PostList({ posts }) {
   return (
-    <div className={styles.card}>
-    
-    {posts.map((post) => {
-     return( <>
-        <h2 className={styles.title}>{post.title}</h2>
-        <p> {post.body}</p>
-
-        </>)
-        console.log(post)
-    })}
-    <p className={styles.text}>Posts and stuff and fish lorem </p>
-</div>
-  )
+    <div>
+      {posts.map((post, index) => (
+        <div className={styles.card} key={index}>
+          <h2 className={styles.title}>{post.title}</h2>
+          <p>{post.body}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
